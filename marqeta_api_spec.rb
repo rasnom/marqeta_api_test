a7a7b6930179c5e05b0560252de0a9b9c997e402
Ransom <ransom.byers@gmail.com>
2017-10-19 14:47:21 -0700

    Add test for sandbox heartbeat

--- marqeta_api_spec.rb
+++ marqeta_api_spec.rb
@@ -1,9 +1,17 @@
 require "httparty"
 
 describe "Marqeta Sandbox and APIs" do
-  describe "Basic Testing" do
+  let(:base_uri) { "https://shared-sandbox-api.marqeta.com/v3" }
+
+  describe "Basic Setup" do
     it "Rspec works" do
       expect(1+1).to eq 2
+    end
+
+    it "Can detect the sandbox heartbeat" do
+      uri = base_uri + "/ping"
+      response = HTTParty.get(uri)
+      expect(response["success"]).to be true
     end
 
   end
