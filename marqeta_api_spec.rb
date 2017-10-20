68a4152d9f42f16ed8da5c36945ee48d5508e48c
Ransom <ransom.byers@gmail.com>
2017-10-19 17:25:28 -0700

    Add test for creating a funding source

--- marqeta_api_spec.rb
+++ marqeta_api_spec.rb
@@ -6,6 +6,7 @@ describe "Marqeta Sandbox and APIs" do
     username: ENV["MARQETA_SANDBOX_APPLICATION_TOKEN"],
     password: ENV["MARQETA_SANDBOX_MASTER_ACCESS_TOKEN"]
   } }
+  let(:headers) { { "content-Type" => "application/json" } }
 
   describe "Basic Operation" do
     it "Can detect the sandbox heartbeat" do
@@ -16,7 +17,6 @@ describe "Marqeta Sandbox and APIs" do
 
     it "Can create a Card Product" do
       uri = base_uri + "/cardproducts"
-      headers = { "content-Type" => "application/json" }
       body =  {
         start_date: "2017-01-01",
         name: "Example Card Product",
@@ -32,8 +32,18 @@ describe "Marqeta Sandbox and APIs" do
           }
         }
       }.to_json
-      response = HTTParty.post(uri, basic_auth: auth, headers: headers, body: body).parsed_response
+      options = { basic_auth: auth, headers: headers, body: body }
+      response = HTTParty.post(uri, options).parsed_response
       expect(response["name"]).to eq "Example Card Product"
+      expect(response["active"]).to be true
+    end
+
+    it "Can create a Program Funding Source" do
+      uri = base_uri + "/fundingsources/program"
+      body = { name: "Program Funding" }.to_json
+      options = { basic_auth: auth, headers: headers, body: body }
+      response = HTTParty.post(uri, options).parsed_response
+      expect(response["name"]).to eq "Program Funding"
       expect(response["active"]).to be true
     end
 
